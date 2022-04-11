c0a48e163330d6e5d1e236cbe9e9c168db1d13e0
katrin1998 <96307671+katrin1998@users.noreply.github.com>
2022-04-12 02:54:32 +0300

    Update crm-sistem.py

--- crm-sistem.py
+++ crm-sistem.py
@@ -1,1 +1,24 @@
 $ python manage.py startapp leads
+leads
+├── __init__.py
+├── admin.py
+├── apps.py
+├── migrations
+│   └── __init__.py
+├── models.py
+├── tests.py
+└── views.py
+
+class Lead(models.Model):
+    name = CharField("название", max_length=255)
+    title = CharField("название работы", max_length=255)
+    contact = CharField("Контакты", max_length=255)
+    email = EmailField("Почта")
+    description = TextField("описывать")
+    attachment = FileField(«Приложение», upload_to="upload")
+    create_time = DateTimeField(«Создание времени», auto_now_add=True)
+    update_time = DateTimeField(«Последнее обновление времени», auto_now=True)
+
+    def __str__(self):
+        return self.name
+      
